9a26c4ca7e6a41bbbbb2844b2ef1067bd4aed1ae
Omelkir <109902709+Omelkir@users.noreply.github.com>
2025-06-09 15:08:07 +0100

    Update connexion.ts

--- src/utils/connexion.ts
+++ src/utils/connexion.ts
@@ -1,9 +1,10 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // Replace with your MySQL username
-  password: '', // Replace with your MySQL password
+  host: '176.31.223.21',
+  port: '3307',
+  user: 'mediconnect', // Replace with your MySQL username
+  password: 'mediconnect', // Replace with your MySQL password
   database: 'medi_connect' // Replace with your MySQL database name
 })
 
